4e0a2655758912bbc55a2ff5ad1fcd4afec6f34c
pasted <tufpaste@hotmail.com>
2013-08-16 01:03:44 +0100

    changes to batch worker

--- app/workers/batch_worker.rb
+++ app/workers/batch_worker.rb
@@ -2,16 +2,30 @@ class BatchWorker
   include Sidekiq::Worker
   include Biomart
   
+  sidekiq_options :retry => 2, :backtrace => true
+  
+  sidekiq_retry_in do |count|
+  	5 * (count + 1)
+  end
+  
+  def convert_strand_string(strand)
+  	  if strand == "-1"
+  	    return "-"
+  	  elsif strand == "1"
+  	    return "+"
+  	  end
+  end
+  
   def perform(variant_id)
     this_variant = Variant.find(variant_id)
     this_location = this_variant.location
     biomart = Biomart::Server.new("http://www.biomart.org/biomart")
     ensembl = biomart.datasets["hsapiens_gene_ensembl"]
     
-    response = ensembl.search(:filters => {"chromosome_name" => this_location.chromosome.name, "start" => this_location.start_position.to_i, "end" => this_location.start_position.to_i+1, "status" => "KNOWN"},
+    response = ensembl.search(:filters => {"chromosome_name" => this_location.chromosome.name, "start" => this_location.position_start.to_i, "end" => this_location.position_start.to_i+1, "status" => "KNOWN"},
 	:attributes => ["external_gene_id", "ensembl_gene_id", "chromosome_name", "strand", "start_position", "end_position", "transcript_count"])
     
-    if response
+    if (response) && !(Gene.find_by_ensembl_gene_id(response[:data][0][1]))
       this_gene = Gene.new
       this_gene.external_gene_id = response[:data][0][0]
       this_gene.ensembl_gene_id  = response[:data][0][1]
